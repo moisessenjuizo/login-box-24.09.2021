06238b2dd0e755265b501dc9b534601626ea4acf
moisessenjuizo <63744932+moisessenjuizo@users.noreply.github.com>
2025-07-18 11:19:46 -0300

    Criar MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,16 @@
+package com.exemplo.consultas;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/index.html");
+        setContentView(webView);
+    }
+}
